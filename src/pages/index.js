f5cfc6dce0f0a89172228d7425d795884856aecd
Carrie Pascale <cpascale43@gmail.com>
2020-10-11 17:17:27 -0400

    test popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useEffect } from "react"
 
+import { popupScript } from "./script";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Body from "../components/body"
@@ -7,12 +8,25 @@ import Footer from "../components/footer"
 
 import "../components/heroImage.css"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Body />
-    <Footer />
-  </Layout>
-)
+const IndexPage = () => {
+  useEffect(() => {
+    const script = document.createElement("script")
+
+    // why does this have to be in template literals?
+
+    script.innerHTML = `${popupScript}`
+    script.type = "text/javascript"
+    script.async = true
+    document.body.appendChild(script)
+    return () => document.body.removeChild(script)
+  }, [])
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Body />
+      <Footer />
+    </Layout>
+  )
+}
 
 export default IndexPage
